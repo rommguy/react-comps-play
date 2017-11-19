9c63633f9ade1d9ba4a2848c96885b4165154a60
rommguy <rommguy@gmail.com>
2017-11-19 21:12:51 +0200

    initial redux app

---
+++ src/appInitialState.js
@@ -0,0 +1,705 @@
+export const initialState = {
+  todos: [],
+  usersData: {
+    "000000": {
+      "id": "000000",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "000101": {
+      "id": "000101",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "000202": {
+      "id": "000202",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "000303": {
+      "id": "000303",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "000404": {
+      "id": "000404",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "000505": {
+      "id": "000505",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "000606": {
+      "id": "000606",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "000707": {
+      "id": "000707",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "000808": {
+      "id": "000808",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "000909": {
+      "id": "000909",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00010010": {
+      "id": "00010010",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00011011": {
+      "id": "00011011",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00012012": {
+      "id": "00012012",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00013013": {
+      "id": "00013013",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00014014": {
+      "id": "00014014",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00015015": {
+      "id": "00015015",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00016016": {
+      "id": "00016016",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00017017": {
+      "id": "00017017",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00018018": {
+      "id": "00018018",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00019019": {
+      "id": "00019019",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00020020": {
+      "id": "00020020",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00021021": {
+      "id": "00021021",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00022022": {
+      "id": "00022022",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00023023": {
+      "id": "00023023",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00024024": {
+      "id": "00024024",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00025025": {
+      "id": "00025025",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00026026": {
+      "id": "00026026",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00027027": {
+      "id": "00027027",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00028028": {
+      "id": "00028028",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00029029": {
+      "id": "00029029",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00030030": {
+      "id": "00030030",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00031031": {
+      "id": "00031031",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00032032": {
+      "id": "00032032",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00033033": {
+      "id": "00033033",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00034034": {
+      "id": "00034034",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00035035": {
+      "id": "00035035",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00036036": {
+      "id": "00036036",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00037037": {
+      "id": "00037037",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00038038": {
+      "id": "00038038",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00039039": {
+      "id": "00039039",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00040040": {
+      "id": "00040040",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00041041": {
+      "id": "00041041",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00042042": {
+      "id": "00042042",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00043043": {
+      "id": "00043043",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00044044": {
+      "id": "00044044",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00045045": {
+      "id": "00045045",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00046046": {
+      "id": "00046046",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00047047": {
+      "id": "00047047",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00048048": {
+      "id": "00048048",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00049049": {
+      "id": "00049049",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00050050": {
+      "id": "00050050",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00051051": {
+      "id": "00051051",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00052052": {
+      "id": "00052052",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00053053": {
+      "id": "00053053",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00054054": {
+      "id": "00054054",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00055055": {
+      "id": "00055055",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00056056": {
+      "id": "00056056",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00057057": {
+      "id": "00057057",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00058058": {
+      "id": "00058058",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00059059": {
+      "id": "00059059",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00060060": {
+      "id": "00060060",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00061061": {
+      "id": "00061061",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00062062": {
+      "id": "00062062",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00063063": {
+      "id": "00063063",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00064064": {
+      "id": "00064064",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00065065": {
+      "id": "00065065",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00066066": {
+      "id": "00066066",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00067067": {
+      "id": "00067067",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00068068": {
+      "id": "00068068",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00069069": {
+      "id": "00069069",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00070070": {
+      "id": "00070070",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00071071": {
+      "id": "00071071",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00072072": {
+      "id": "00072072",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00073073": {
+      "id": "00073073",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00074074": {
+      "id": "00074074",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00075075": {
+      "id": "00075075",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00076076": {
+      "id": "00076076",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00077077": {
+      "id": "00077077",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00078078": {
+      "id": "00078078",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00079079": {
+      "id": "00079079",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00080080": {
+      "id": "00080080",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00081081": {
+      "id": "00081081",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00082082": {
+      "id": "00082082",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00083083": {
+      "id": "00083083",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00084084": {
+      "id": "00084084",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00085085": {
+      "id": "00085085",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00086086": {
+      "id": "00086086",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00087087": {
+      "id": "00087087",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00088088": {
+      "id": "00088088",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00089089": {
+      "id": "00089089",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00090090": {
+      "id": "00090090",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00091091": {
+      "id": "00091091",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00092092": {
+      "id": "00092092",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00093093": {
+      "id": "00093093",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00094094": {
+      "id": "00094094",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00095095": {
+      "id": "00095095",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00096096": {
+      "id": "00096096",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00097097": {
+      "id": "00097097",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00098098": {
+      "id": "00098098",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    },
+    "00099099": {
+      "id": "00099099",
+      "name": "",
+      "lastName": "",
+      "age": 1,
+      "permissions": 0
+    }
+  }
+}
